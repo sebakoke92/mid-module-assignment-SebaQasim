9c42f2829a3d354866431ee489d589be71899f8f
sebakoke92 <116904089+sebakoke92@users.noreply.github.com>
2022-11-27 23:02:50 +0300

    Update floyed warshal code.py

--- floyed warshal code.py
+++ floyed warshal code.py
@@ -59,6 +59,30 @@ if __name__ == "__main__":
     graph = [[0, 7, INF, 8], [INF, 0, 5, INF], [INF, INF, 0, 2], [INF, INF, INF, 0]]
     #  call Floyd Warshall function
     floydWarshall(graph)
+    
+    
+    # unit test case
+import unittest
+
+class TestStringMethod (unittest.TestCase):
+    # test function to test equality of two value 
+    def test_positive(self):
+        output_list =graph
+        expected list =[
+            [0,7,12,8],
+            [INF,0,5,7],
+            [INF,INF,0,2],
+            [INF,INF,INF,0],
+        ]
+
+        # error message in case if test case got failed
+        message = " First value and second value are not equal  !"
+        #assertEqual() to check equality of first & second value 
+        self.assertEqual(expected_list, output_list)
+
+if __name__=== "__main__":
+     unittest.main()
+
 
  
 
